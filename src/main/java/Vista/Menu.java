c80771117912d8e0e2c6b8c84a74cc68464c530a
Samuel <samg.martinez2@gmail.com>
2024-06-30 13:37:03 -0300

    Se hizo el menu principal, falta hacer los actionListener correspondientes

--- src/main/java/Vista/Menu.java
+++ src/main/java/Vista/Menu.java
@@ -15,19 +15,68 @@ private JMenuItem funDrama, funRomance, funTerror, funBiografica, funSuspenso;
         setSize(500,500);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
+        //setLayout();
 
         // iniciar el menu bar y asociarlo al frame //
         navbar = new JMenuBar();
         setJMenuBar(navbar);
+
         //Iniciarlizar menus y agregarlos al main  //
         crearPelicula = new JMenu("Crear nueva Pelicula");
         navbar.add(crearPelicula);
         crearfuncion = new JMenu("Crear nueva Funcion");
         navbar.add(crearfuncion);
-        // Inicializamos los menus Item y agregamos
+
+        // Inicializamos los menus Items de peliculas, y le decimos el texto que va a mostrar //
+        pelDrama = new JMenuItem("Drama");
+        pelRomance = new JMenuItem("Romance");
+        pelTerror = new JMenuItem("Terror");
+        pelBiografica = new JMenuItem("Biografica");
+        pelSuspenso = new JMenuItem("Suspenso");
+
+        //  Le agregamos el actionListener a cada uno  //
+        pelDrama.addActionListener(this);
+        pelRomance.addActionListener(this);
+        pelTerror.addActionListener(this);
+        pelBiografica.addActionListener(this);
+        pelSuspenso.addActionListener(this);
+
+        // Se agregan los JmenuItem a su menu correspondiente  //
+        crearPelicula.add(pelDrama);
+        crearPelicula.add(pelRomance);
+        crearPelicula.add(pelTerror);
+        crearPelicula.add(pelBiografica);
+        crearPelicula.add(pelSuspenso);
+
+        //  Repetimos el proceso con los Jmenu de Funciones  //
+        funDrama = new JMenuItem("Drama");
+        funRomance = new JMenuItem("Romance");
+        funTerror = new JMenuItem("Terror");
+        funBiografica = new JMenuItem("Biografica");
+        funSuspenso = new JMenuItem("Suspenso");
+
+        funDrama.addActionListener(this);
+        funRomance.addActionListener(this);
+        funTerror.addActionListener(this);
+        funBiografica.addActionListener(this);
+        funSuspenso.addActionListener(this);
+
+        crearfuncion.add(funDrama);
+        crearfuncion.add(funRomance);
+        crearfuncion.add(funTerror);
+        crearfuncion.add(funBiografica);
+        crearfuncion.add(funSuspenso);
+
+
+
+
+        setVisible(true);
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+    public static void main(String[] args){
+        new Menu();
     }
 }
